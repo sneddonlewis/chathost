b6656936bd054c330a6f28afd78f60ef089a326c
Lewis Sneddon <lewis@lewiscodes.io>
2022-10-21 19:53:16 +0100

    default host and port

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Client Started");
-    let host = env!("HOST");
-    let port = env!("PORT");
+    let host = option_env!("HOST").unwrap_or_else(|| "localhost");
+    let port = option_env!("PORT").unwrap_or_else(|| "12345");
     println!("Host {}, Port {}", host, port);
 }
