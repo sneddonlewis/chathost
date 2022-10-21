4f05f8924bdad4c0c0940e8f38f9f381e5783ba3
Lewis Sneddon <lewis@lewiscodes.io>
2022-10-21 21:00:07 +0100

    tcp connection between client and echo server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,46 @@
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
+
 use chathost::config;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50];
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        }
+        Err(_) => {
+            println!(
+                "An error occurred, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
 
 fn main() {
     println!("Server Started");
     let host = config::get_host();
     let port = config::get_port();
+    let address = config::get_address();
     println!("Server on host {}, port {}", host, port);
+
+    let listener = TcpListener::bind(address).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || handle_client(stream));
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    drop(listener);
 }
